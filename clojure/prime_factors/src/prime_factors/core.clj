8cef156336833a766a02769b2d259fc5e4822d20
Paul Middlin <p.middlin@techsmith.com>
2017-01-10 10:16:54 -0500

    clojure

--- clojure/prime_factors/src/prime_factors/core.clj
+++ clojure/prime_factors/src/prime_factors/core.clj
@@ -1,4 +1,13 @@
 (ns prime-factors.core)
 
 (defn primes
-  ([num] []))
+   ([num] (primes num 2))
+   ([num candidate]
+      (cond
+         (< num candidate) []
+         (= (mod num candidate) 0)
+            (cons candidate (primes (/ num candidate) candidate) )
+         :else (primes num (inc candidate))
+      )
+   )
+)
--- clojure/prime_factors/test/prime_factors/core_test.clj
+++ clojure/prime_factors/test/prime_factors/core_test.clj
@@ -1,6 +1,16 @@
 (ns prime-factors.core-test
-  (:require [clojure.test :refer :all]
+   (:require [clojure.test :refer :all]
             [prime-factors.core :refer :all]))
 
 (deftest prime-factors
-  (is (= (primes 1) [])))
+   (is (= (primes 1) []))
+   (is (= (primes 2) [2]))
+   (is (= (primes 3) [3]))
+   (is (= (primes 4) [2,2]))
+   (is (= (primes 6) [2,3]))
+   (is (= (primes 8) [2,2,2]))
+   (is (= (primes 9) [3,3]))
+;; (is (= (primes 36) [2,2,3,3]))
+
+)
+
