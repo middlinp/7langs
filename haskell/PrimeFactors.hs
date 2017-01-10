364c4efbef470288b96d4771c61e0d0beb94fd8b
Paul Middlin <p.middlin@techsmith.com>
2017-01-10 11:20:40 -0500

    Haskell

--- haskell/PrimeFactors.hs
+++ haskell/PrimeFactors.hs
@@ -1,4 +1,8 @@
 module PrimeFactors (primeFactors) where
 
 primeFactors :: Integer -> [Integer]
-primeFactors x = []
+primeFactors x = primeFactorsTwoArgs x 2
+   where primeFactorsTwoArgs x c
+            | x < c = []
+            | x `mod ` c == 0 = [c] ++ primeFactorsTwoArgs (x `div` c) c
+            | otherwise = primeFactorsTwoArgs x (c + 1)
